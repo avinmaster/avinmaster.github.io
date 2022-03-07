e6c8e0460a5e40e526c1c4a6d0e4d7da6d2e3be7
avinmaster <oybek.odilov.dev@gmail.com>
2022-03-07 18:17:18 +0500

    Replaced fullpage-scroll with nuxt-fullpage

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,9 +20,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    { src: '~/plugins/vue-fullpage', mode: 'client' }
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
