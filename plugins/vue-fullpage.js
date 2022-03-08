94511375308a4a445d3cc8c1cd1334e377cc5066
avinmaster <oybek.odilov.dev@gmail.com>
2022-03-08 14:24:51 +0500

    Added styles

--- plugins/vue-fullpage.js
+++ plugins/vue-fullpage.js
@@ -2,5 +2,6 @@ import Vue from 'vue'
 import 'fullpage.js/dist/fullpage.extensions.min'
 import 'fullpage.js/vendors/scrolloverflow'
 import VueFullPage from 'vue-fullpage.js'
+import 'fullpage.js/dist/fullpage.min.css'
 
 Vue.use(VueFullPage)
